2718185899139fd67cf48fafa5d9b8db22ae088e
rafail9g <rafaaulia668@gmail.com>
2025-09-24 18:06:39 +0700

    Add files via upload

---
+++ porto.js
@@ -0,0 +1,21 @@
+
+const aboutSection = document.getElementById("about");
+aboutSection.innerHTML = `
+  <div class="about-content">
+    <h2>About Me</h2>
+    <p>
+      Aku Rafail, mahasiswa Universitas Jember jurusan Teknologi Informasi.
+    </p>
+    <img src="img/p_info.gif" alt="Foto About Me">
+  </div>
+`
+
+const projectsSection = document.getElementById("projects");
+projectsSection.innerHTML = `
+  <h2>My Projects</h2>
+  <div class="project-cards">
+    <img src="img/aaq.jpg" alt="Project 1">
+    <img src="img/komando.jpg" alt="Project 2">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/d/dd/Sukhoi_Su-47_in_2007.jpg" alt="Project 3">
+  </div>
+`
